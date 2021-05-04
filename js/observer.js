98610a1700c7e58a83b32de11735a3f5fd6db1c7
Guilherme <grampelottorodrigues@gmail.com>
2021-05-03 22:11:17 -0300

    first commit

---
+++ js/observer.js
@@ -0,0 +1,43 @@
+const nav = document.querySelector("nav");
+const targets = document.querySelectorAll("section");
+
+const sectionOptions = {
+  threshold: 0,
+  rootMargin: "-6.5% 0px -93% 0px",
+};
+
+const sectionObserver = new IntersectionObserver(function (
+  entries,
+  sectionObserver
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else if (entry.target.classList.contains("nav-bg-black-target")) {
+      nav.classList.add("nav-bg-black");
+      nav.classList.remove("nav-bg-white");
+      nav.classList.remove("nav-bg-light");
+      nav.classList.remove("nav-bg-dark");
+    } else if (entry.target.classList.contains("nav-bg-dark-target")) {
+      nav.classList.add("nav-bg-dark");
+      nav.classList.remove("nav-bg-white");
+      nav.classList.remove("nav-bg-light");
+      nav.classList.remove("nav-bg-black");
+    } else if (entry.target.classList.contains("nav-bg-light-target")) {
+      nav.classList.add("nav-bg-light");
+      nav.classList.remove("nav-bg-white");
+      nav.classList.remove("nav-bg-black");
+      nav.classList.remove("nav-bg-dark");
+    } else if (entry.target.classList.contains("nav-bg-white-target")) {
+      nav.classList.add("nav-bg-white");
+      nav.classList.remove("nav-bg-black");
+      nav.classList.remove("nav-bg-light");
+      nav.classList.remove("nav-bg-dark");
+    }
+  });
+},
+sectionOptions);
+
+targets.forEach((section) => {
+  sectionObserver.observe(section);
+});
